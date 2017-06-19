31ad9cc54ad81061fae999fda5a3133f94eb49bc
Brittany Chiang <brittany.chiang@gmail.com>
2017-06-19 00:30:22 -0400

    some js for dropdowns

--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
   // get current URL path and assign 'active' class
-  var pathname = window.location.pathname;
+  const pathname = window.location.pathname;
   $('nav li > a[href="'+pathname+'"]').addClass('active');
+
+  const selectedContinent = $("#continent :selected").text();
+  const selectedClimate = $("#climate :selected").text();
+  const selectedCountry = $("#country :selected").text();
+  const selectedCategory = $("#category :selected").text();
+  const selectedOrigin = $("#origin :selected").text();
+
+  $('#filter').click( () => {
+
+    const filters = [
+      selectedContinent,
+      selectedClimate,
+      selectedCountry,
+      selectedCategory,
+      selectedOrigin
+    ]
+
+    console.log(filters);
+
+  });
+
+
 })
