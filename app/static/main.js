d06d66fb33b5ca645117498406f3d22bead9666f
Brittany Chiang <brittany.chiang@gmail.com>
2017-06-21 18:24:52 -0400

    use local storage to set visitorID

--- app/static/main.js
+++ app/static/main.js
@@ -3,14 +3,35 @@ $(document).ready(function() {
   const pathname = window.location.pathname;
   $('nav li > a[href="'+pathname+'"]').addClass('active');
 
-  $.getJSON('http://api.fixer.io/latest?base=USD', function(data) {
-    console.log(data);
-  });
+  // $.getJSON('http://api.fixer.io/latest?base=USD', function(data) {
+  //   console.log(data);
+  // });
 
   // set hidden input value for review deletion
   $('.delete').click(function() {
     id = $(this).data('id');
     $('#deleteReviewModal input.form-control').val(id);
   });
+
+  // if visitor ID does not exist in set visitorID in local storage after adding a visitor
+  if (pathname == '/profile') {
+    visitorID = $('#visitorid').val()
+    localStorage.setItem('visitorID', visitorID);
+  }
+
+  // if visitor ID exists in local storage
+  if (localStorage.getItem('visitorID') !== null) {
+    // show ID in nav bar
+    visitorID = localStorage.getItem('visitorID');
+    $('#navIDNum').text(visitorID);
+
+    // auto fill visitor ID field then writing a review
+    if (pathname == '/review') {
+      visitorID = localStorage.getItem('visitorID');
+      $('#visitorID').val(visitorID);
+    }
+  }
+
+
 
 })
